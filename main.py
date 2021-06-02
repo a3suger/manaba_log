b75e2cf213d29dcfbd764a0d865e7f76af6359e1
Akira SATO <akira@cc.tsukuba.ac.jp>
2021-06-02 10:38:27 +0900

    refine command line argument

--- main.py
+++ main.py
@@ -2,8 +2,12 @@ import chromedriver_binary
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import selenium.common.exceptions
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions
 
 import configparser
+import argparse
+
 
 def one_parson(d, name, url):
     while True:
@@ -13,7 +17,7 @@ def one_parson(d, name, url):
             items = row.find_elements_by_tag_name('td')
             if items is not None and len(items) > 4:
                 print(','.join([name, items[0].get_attribute('title'), items[1].get_attribute('title'),
-                                      items[2].text, items[3].text]))
+                                items[2].text, items[3].text]))
         try:
             _next = d.find_element_by_id('AFHasNext')
         except selenium.common.exceptions.NoSuchElementException as e:
@@ -21,21 +25,37 @@ def one_parson(d, name, url):
         url = _next.get_attribute('href')
 
 
-config = configparser.ConfigParser()
-config.read('config.ini', encoding='utf-8')
-config_manaba = config['manaba']
+def main_parse(d):
+    list = []
+    for a in d.find_element_by_tag_name('table').find_elements_by_tag_name('a'):
+        list.append(a.get_attribute('href'))
+    for item in list:
+        name = item.split('_')[-1]
+        one_parson(d, name, item)
 
-driver = webdriver.Chrome()
-driver.get("https://{}/ct/course_{}_footprint".format(config_manaba.get('host'),config_manaba.get('cource'))) #1931824
 
-driver.find_element_by_id('username').send_keys(config_manaba.get('username'))
-driver.find_element_by_id('password').send_keys(config_manaba.get('password'))
-driver.find_element_by_tag_name('button').click()
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('host', help='hostname of manaba')
+    parser.add_argument('cource', help='cource number of manaba')
 
-list = []
-for a in driver.find_element_by_tag_name('table').find_elements_by_tag_name('a'):
-    list.append(a.get_attribute('href'))
-for item in list:
-    name = item.split('_')[-1]
-    one_parson(driver, name, item)
+    parser.add_argument('--config', help='config file for usernema and password')
+    parser.add_argument('--timeout', help='timeout (Unit:sec)', default=30, type=int)
 
+    args = parser.parse_args()
+
+    driver = webdriver.Chrome()
+    driver.get("https://{}/ct/course_{}_footprint".format(args.host, args.cource))  # 1931824
+
+    if args.config is not None:
+        config = configparser.ConfigParser()
+        config.read(args.config, encoding='utf-8')
+        config_manaba = config['manaba']
+        driver.find_element_by_id('username').send_keys(config_manaba.get('username'))
+        driver.find_element_by_id('password').send_keys(config_manaba.get('password'))
+        driver.find_element_by_tag_name('button').click()
+    else:
+        print("Please login until {} sec".format(args.timeout))
+        existTitle = WebDriverWait(driver, args.timeout).until(expected_conditions.title_contains("manaba"))
+
+    main_parse(driver)
